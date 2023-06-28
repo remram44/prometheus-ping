2441c7bfff6367c4f6c5eb5c07d9777a10dcde66
Remi Rampin <remi@rampin.org>
2023-06-28 11:47:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Prometheus ping metrics
+=======================
+
+This system measures network latency between its instances. You can deploy it in multiple servers or data centers and collect the latency and packet loss information via Prometheus.
+
+Each instance is given a **source name** (reported in the metrics), a **UDP port** (for other instances to ping), and a **list of targets** (that will be pinged every second).
+
+For example:
+
+```
+python prometheus_ping.py --source spire:5000 --listen-port 5000 database:5555
+python prometheus_ping.py --source database:5555 --listen-port 5555 spire:5000
+```
+
+The resulting metrics look like:
+
+```
+ping_packet_loss_total{source="spire:5000",target="database:5555"} 123.0
+ping_packet_loss_total{source="database:5555",target="spire:5000"} 27.0
+ping_latency_average_30s{source="spire:5000",target="database:5555"} 0.00123
+ping_latency_average_30s{source="database:5555",target="spire:5000"} 0.00130
+```
---
+++ prometheus_ping.py
@@ -0,0 +1,177 @@
+import argparse
+import logging
+import prometheus_client
+import socket
+import struct
+import threading
+import time
+
+
+__version__ = '0.1.0'
+
+
+logger = logging.getLogger('prometheus_ping')
+
+
+SAMPLES = 30
+
+
+def current_time():
+    """Return perf_counter_ns() as a 64-bit binary buffer.
+    """
+    return struct.pack('>Q', time.perf_counter_ns())
+
+
+def get_delay(buf):
+    """Return the elapsed time from the given message.
+    """
+    now = time.perf_counter_ns()
+    orig, = struct.unpack('>Q', buf)
+    return now - orig
+
+
+def parse_target(target):
+    host, port = target.split(':')
+    params = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_DGRAM)
+    return params[0][4]
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser(
+        'prometheus_ping',
+        description=(
+            "Measure ping between locations and report as Prometheus metrics"
+        ),
+    )
+    parser.add_argument('--metrics-port', default=8000, type=int)
+    parser.add_argument('--listen-port', default=5000, type=int)
+    parser.add_argument('--source', required=True)
+    parser.add_argument('target', nargs='*')
+    args = parser.parse_args()
+
+    logger.info("Starting prometheus_ping %s", __version__)
+    logger.info("Source: %s", args.source)
+
+    # Resolve targets
+    logger.info("Targets:")
+    targets = {}
+    for target in args.target:
+        addr = parse_target(target)
+        targets[addr] = target
+        logger.info("  %s -> %s:%s", target, addr[0], addr[1])
+    if not targets:
+        logger.info("  no targets")
+
+    # Create echo server (for others to ping)
+    server_thread = threading.Thread(
+        target=server,
+        args=[args.listen_port],
+        name="UDP echo server",
+        daemon=True,
+    )
+    server_thread.start()
+
+    # Create local socket (to ping)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('0.0.0.0', 0))
+
+    # Setup Prometheus expositing
+    collector = Collector(args.source, targets)
+    prometheus_client.REGISTRY.register(collector)
+    logger.info("Starting Prometheus server on port %d", args.metrics_port)
+    prometheus_client.start_wsgi_server(args.metrics_port)
+
+    # Create ping thread
+    ping_thread = threading.Thread(
+        target=ping,
+        args=[sock, targets, collector.send_counters],
+        name="Ping sender",
+        daemon=True,
+    )
+    ping_thread.start()
+
+    # Receive replies and compute metrics
+    receive(sock, targets, collector)
+
+
+def server(listen_port):
+    logger.info("Starting UDP echo server on port %d", listen_port)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('0.0.0.0', listen_port))
+
+    while True:
+        data, addr = sock.recvfrom(1024)
+        sock.sendto(data, addr)
+
+
+def ping(sock, targets, send_counters):
+    while True:
+        time.sleep(1)
+        for (addr, target) in targets.items():
+            sock.sendto(current_time(), addr)
+            send_counters[target] += 1
+
+
+class Collector(object):
+    def __init__(self, source, targets):
+        self.source = source
+        self.send_counters = {target: 0 for target in targets.values()}
+        self.recv_counters = {target: 0 for target in targets.values()}
+        self.latencies = {target: [] for target in targets.values()}
+        self.last_reply = {target: 0 for target in targets.values()}
+
+    def collect(self):
+        now = time.perf_counter()
+
+        m_packet_loss = prometheus_client.metrics_core.CounterMetricFamily(
+            'ping_packet_loss', "Lost packets",
+            labels=['source', 'target'],
+        )
+
+        # Packet loss is the number of queries minus the number of replies
+        # However we have to account for the last reply which might be on the
+        # way
+        # Unless we got a reply less than a second ago, discount one
+        for target, sent in self.send_counters.items():
+            received = self.recv_counters[target]
+            loss = sent - received
+            if now - self.last_reply[target] > 1.0:
+                loss -= 1
+            m_packet_loss.add_metric([self.source, target], loss)
+
+        m_latency_avg = prometheus_client.metrics_core.GaugeMetricFamily(
+            'ping_latency_average_30s', "Average round-trip latency over the last 30s",
+            labels=['source', 'target'],
+        )
+
+        for target, latencies in self.latencies.items():
+            if latencies:
+                latency = sum(latencies) / len(latencies) / 1e9
+                m_latency_avg.add_metric([self.source, target], latency)
+
+        return [
+            m_packet_loss,
+            m_latency_avg,
+        ]
+
+
+def receive(sock, targets, collector):
+    while True:
+        data, addr = sock.recvfrom(1024)
+        try:
+            target = targets[addr]
+        except KeyError:
+            continue
+        collector.recv_counters[target] += 1
+        collector.last_reply[target] = time.perf_counter()
+        delay = get_delay(data)
+        latencies = collector.latencies[target]
+        latencies.append(delay)
+        if len(latencies) > SAMPLES:
+            latencies[:] = latencies[-SAMPLES:]
+
+
+if __name__ == '__main__':
+    main()
